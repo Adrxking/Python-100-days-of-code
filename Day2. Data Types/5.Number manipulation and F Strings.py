cf0bec3267e7dfe581d32a8bcf81edb1e02db1f3
Adrianas_Vitys <adrianalbujaira@gmail.com>
2021-12-16 13:16:54 +0100

    Number manipulation and F strings

---
+++ Day2. Data Types/5.Number manipulation and F Strings.py
@@ -0,0 +1,18 @@
+# This file contains how we can manipulate numbers and F Strings
+
+# Get the round with 2 decimals
+result = round(8/3, 2)
+
+# For divide by 2 again
+result /= 2
+
+# We can do the following instructions too
+# result += 1
+# result -= 1
+# result *= 1
+# result /= 1
+# result **= 1
+# result %= 1
+
+# F String --> Formatted string literals
+print(f"your score is {result}")
