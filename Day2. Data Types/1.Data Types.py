60c878fac7cd98992312209029aa4c3363e5aa53
Adrianas_Vitys <adrianalbujaira@gmail.com>
2021-12-16 09:32:10 +0100

    Data Types

---
+++ Day2. Data Types/1.Data Types.py
@@ -0,0 +1,19 @@
+# Data Types
+
+# Strings
+
+print("Hello"[4])
+
+#Integers
+
+print(123 + 456)
+
+123_456_789 # The program delete the low bar, we can use the low bars to understand better the number
+
+#Float
+
+3.1416
+
+# Boolean
+True
+False
