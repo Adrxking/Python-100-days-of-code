6f5fc69356f3d75477504a8babda4a98836de885
Adrianas_Vitys <adrianalbujaira@gmail.com>
2021-12-16 09:21:47 +0100

    Day 1. Manage of variables and data

---
+++ Day1. Variables and Manage of Data/1.Print and inputs.py
@@ -0,0 +1,41 @@
+# Get Hello and our name with input
+# First of all it outputs the input, we write the input and then we get the print with the input replaced.  
+print("Exercise 1")
+print("Hello " + input("Write your name... "))
+
+print("")
+print("")
+print("")
+
+# Get the number of characters of our name
+print("Exercise 2")
+print( "The number of characters of your name is -> " + str( len( input("Write your name -> ") ) ) )
+
+print("")
+print("")
+print("")
+
+# Save data of input into variables
+print("Exercise 3")
+name = input("Write your name -> ")
+print(name)
+
+print("")
+print("")
+print("")
+
+# Switch two variables data
+print("Exercise 4")
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print ("a: " + a)
+print ("a: " + b)
+
+print("")
+print("")
+print("")
---
+++ Day1. Variables and Manage of Data/2.Band Name Generator.py
@@ -0,0 +1,13 @@
+# This code is a band name generator that combains
+# the city where your born and the name of your pet 
+
+# 1. Create the program
+print("Welcome to my Band Name Generator")
+# 2. Ask for the city
+city = input ("Please, write the name of the city where you grew up in\n")
+# 3. Ask for the name of the pet
+pet = input ("Please, write the name of your pet\n")
+# 4. Combine the name of the city and the pet
+band_name = city + " " + pet
+# 5. Show the name of the band
+print (band_name)
